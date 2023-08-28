f1e2a2f854cafbf50a82f1d87b9e83e887e4c297
Nikita Levushkin <therederickmind@gmail.com>
2023-08-28 17:34:59 +0300

    Fixed output, fixed data default dict

--- pep_parse/spiders/pep.py
+++ pep_parse/spiders/pep.py
@@ -9,28 +9,25 @@ class PepSpider(scrapy.Spider):
     start_urls = ['https://peps.python.org/']
 
     def parse(self, response):
-        table = response.css('section.numerical-index')
+        table = response.css("section[id='numerical-index']")
         all_peps = table.css('tbody')
-        all_peps_links = all_peps.css('a[href^="pep-"]')
+        all_peps_links = all_peps.css('a::attr(href)').getall()
         for pep_link in all_peps_links:
-            pep_link = pep_link + '/'
             yield response.follow(pep_link, callback=self.parse_pep)
 
     def parse_pep(self, response):
-        pep_info = response.css('section.pep-content')
+        pep_info = response.css("section[id='pep-content']")
         number, name = (
             pep_info.css(
                 'h1.page-title::text'
             ).get().split(' – ', 1)
         )
-        pep_status = pep_info.css(
-            'dt:contains("Status") + dd::text'
-        ).get()
+        pep_status = pep_info.css('abbr::text').get()
 
         pep_data = {
             'number': number.split()[1],
             'name': name,
             'status': pep_status
         }
-
+        print(pep_data)
         yield PepParseItem(pep_data)
